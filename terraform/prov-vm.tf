055bd5d812fbb05eb3531e332b463ab5a3d459e1
hoyby <54535198+Hoyby@users.noreply.github.com>
2023-04-11 20:11:36 +0200

    Improve tf scripts

--- terraform/prov-vm.tf
+++ terraform/prov-vm.tf
@@ -3,16 +3,30 @@ resource "proxmox_vm_qemu" "init_masters" {
   target_node = var.pm_node_name
   name        = "k3s-master-${count.index}"
   vmid        = var.master_vmid + count.index
-
-  clone = var.template_vm_name
+  clone       = var.template_vm_name
 
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
+  oncreate = true
 
-  os_type   = "cloud-init"
-  ipconfig0 = "ip=${var.master_ips[count.index]}/${var.networkrange},gw=${var.networkgateway}"
-
+  os_type    = "cloud-init"
+  ciuser     = var.ciuser
+  cipassword = var.cipassword
+  ipconfig0  = "ip=${var.master_ips[count.index]}/${var.networkrange},gw=${var.networkgateway}"
   nameserver = var.nameserver
+  sshkeys    = <<EOF
+    ${var.ssh_public_key}
+    EOF
+
+  lifecycle {
+    ignore_changes = [
+      disk,
+      network,
+      scsihw,
+      bootdisk,
+      qemu_os,
+    ]
+  }
 }
 
 resource "proxmox_vm_qemu" "init_workers" {
@@ -20,14 +34,28 @@ resource "proxmox_vm_qemu" "init_workers" {
   target_node = var.pm_node_name
   name        = "k3s-worker-${count.index}"
   vmid        = var.worker_vmid + count.index
-
-  clone = var.template_vm_name
+  clone       = var.template_vm_name
 
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
+  oncreate = true
 
-  os_type   = "cloud-init"
-  ipconfig0 = "ip=${var.worker_ips[count.index]}/${var.networkrange},gw=${var.networkgateway}"
-
+  os_type    = "cloud-init"
+  ciuser     = var.ciuser
+  cipassword = var.cipassword
+  ipconfig0  = "ip=${var.worker_ips[count.index]}/${var.networkrange},gw=${var.networkgateway}"
   nameserver = var.nameserver
+  sshkeys    = <<EOF
+    ${var.ssh_public_key}
+    EOF
+
+  lifecycle {
+    ignore_changes = [
+      disk,
+      network,
+      scsihw,
+      bootdisk,
+      qemu_os
+    ]
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,6 @@
 variable "proxmox_api_url" {
-  type = string
+  type      = string
+  sensitive = true
 }
 
 variable "proxmox_api_token_id" {
@@ -27,37 +28,61 @@ variable "proxmox_api_token_secret" {
 # }
 
 variable "pm_node_name" {
-  type = string
+  type    = string
+  default = "pve"
 }
 
 variable "template_vm_name" {
-  type = string
+  type    = string
+  default = "ubuntu-cloud"
 }
 
 variable "master_vmid" {
-  type = number
+  type    = number
+  default = 100
+}
+
+variable "ciuser" {
+  type    = string
+  default = "admin"
+}
+
+variable "cipassword" {
+  type      = string
+  sensitive = true
 }
 
 variable "master_ips" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.0.100"]
 }
 
 variable "worker_vmid" {
-  type = number
+  type    = number
+  default = 110
 }
 
 variable "worker_ips" {
-  type = list(string)
+  type    = list(string)
+  default = ["10.0.0.110", "10.0.0.111", "10.0.0.112"]
 }
 
 variable "networkrange" {
-  type = string
+  type    = string
+  default = "24"
 }
 
 variable "networkgateway" {
-  type = string
+  type    = string
+  default = "10.0.0.1"
 }
 
 variable "nameserver" {
-  type = string
+  type    = string
+  default = ""
+}
+
+variable "ssh_public_key" {
+  type      = string
+  sensitive = true
 }
